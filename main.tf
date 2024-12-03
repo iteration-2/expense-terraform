ccfedb37d35e0d794ecf2790fa545a09cec0c4e3
pappik <paparao.prema75@gmail.com>
2024-12-03 14:24:55 -0500

    ssh spelling corrected

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "frontend" {
   source        = "./modules/app"
   instance_type = var.instance_type
   component     = frontend
-  shh_user      = var.ssh_user
-  shh_pass      = var.ssh_pass
+  ssh_user      = var.ssh_user
+  ssh_pass      = var.ssh_pass
   env           = var.env
   zone_id       = var.zone_id
 }
@@ -13,8 +13,8 @@ module "backend" {
   source        = "./modules/app"
   instance_type = var.instance_type
   component     = backend
-  shh_user      = var.ssh_user
-  shh_pass      = var.ssh_pass
+  ssh_user      = var.ssh_user
+  ssh_pass      = var.ssh_pass
   env           = var.env
   zone_id       = var.zone_id
 }
@@ -23,8 +23,8 @@ module "mysql" {
   source        = "./modules/app"
   instance_type = var.instance_type
   component     = mysql
-  shh_user      = var.ssh_user
-  shh_pass      = var.ssh_pass
+  ssh_user      = var.ssh_user
+  ssh_pass      = var.ssh_pass
   env           = var.env
   zone_id       = var.zone_id
 }
