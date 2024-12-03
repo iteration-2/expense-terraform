e7d1b051c7805186a1805caffd6b0de414b99336
pappik <paparao.prema75@gmail.com>
2024-12-03 14:29:21 -0500

    setting variable default value as null

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
 variable "instance_type" {}
 variable "env" {}
-variable "component" {}
+variable "component" {
+  default = null
+}
 variable "ssh_user" {}
 variable "ssh_pass" {}
 variable "zone_id" {}
